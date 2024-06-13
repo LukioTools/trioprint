998b4919842b659cb4202d2c0d624a5936f3f413
Pikku Hurja <pikku.hurja@gmail.com>
2024-06-14 00:07:55 +0300

    quick commit -_-

--- Handlers.h
+++ Handlers.h
@@ -13,7 +13,7 @@
 #include "H-UploadFile.h"
 
 namespace WebServerW {
-    void handle(){server.handle();}
+    inline static void handle(){server.handle();}
     
     void begin(){
         server.on("/", HTTP_GET ,Handlers::Root);
