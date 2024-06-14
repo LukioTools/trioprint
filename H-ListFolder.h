647391d7ee1473e074b9947cd622ad88e81a1f38
Pikku Hurja <pikku.hurja@gmail.com>
2024-06-14 15:35:52 +0300

    quick commit -_-

--- H-ListFolder.h
+++ H-ListFolder.h
@@ -4,6 +4,7 @@ using namespace WebServerW;
 namespace Handlers {
     void ListFolder(){
         Serial.println(server.arg("plain"));
-        server.send(200, "application/json", SDW::listDir(server.arg("plain")).c_str());
+        auto e = SDW::listDir(server.arg("plain"));
+        server.send(200, "application/json", e.c_str(), e.length);
     }
 }
