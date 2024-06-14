f58e293d3e61ac400bc5577835678f3dc8b263e8
Pikku Hurja <pikku.hurja@gmail.com>
2024-06-14 16:06:36 +0300

    quick commit -_-

--- Handlers.h
+++ Handlers.h
@@ -31,7 +31,9 @@ namespace WebServerW {
         server.on("/fm/remove/", HTTP_GET ,Handlers::Remove);
         server.on("/fm/mkdir/", HTTP_GET ,Handlers::Mkdir);
         server.on("/fm/downloadFile/", HTTP_GET, Handlers::DownloadFile);
-        server.on("/fm/uploadFile/", HTTP_POST, Handlers::UploadFile);
+        server.on("/fm/uploadFile/", HTTP_POST,
+            [](){ server.send(200); },                          // Send status 200 (OK) to tell the client we are ready to receive
+        Handlers::UploadFile);
 
         server.onNotFound(Handlers::NotFound);
 
