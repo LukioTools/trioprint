1575456ca27d535154d512156f6f0cfeeb5f6e71
Vili <lukio.tools@gmail.com>
2024-06-14 15:25:44 +0300

    quick commit -_-

--- H-DownloadFile.h
+++ H-DownloadFile.h
@@ -6,8 +6,9 @@ namespace Handlers {
     server.sendHeader("Content-Encoding", "application/octet-stream");
     Serial.println("downloading file");
     size_t filesize = 0;
-    const String& name = server.arg("fileName");
+    const String& name = server.arg("filename");
     uint8_t* filedata = SDW::readFile(name.c_str(), filesize);
+    server.sendHeader("Content-Disposition", "inline; filename=\""+ name + '"');
     server.send(200, "application/octet-stream", (char *)filedata, filesize);
     Serial.println("Download completed");
   }
