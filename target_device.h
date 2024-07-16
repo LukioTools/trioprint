9e3f128c932addcad7f1c9d235700166ce1bb761
Vili <lukio.tools@gmail.com>
2024-07-16 11:36:59 +0300

    quick commit -_-

--- target_device.h
+++ target_device.h
@@ -41,6 +41,7 @@ namespace TD {
 
     bool isPrintting = false;
 
+<<<<<<< HEAD
     class GCode{
         String filename;
         FsFile file;
@@ -83,6 +84,28 @@ namespace TD {
 
 
     }; 
+=======
+    bool firmware(){ // M115
+        serial.println();
+        //return "FIRMWARE_NAME:Marlin V1.0.6 (Sep 19 2023 09:14:17) SOURCE_CODE_URL:github.com/MarlinFirmware/Marlin PROTOCOL_VERSION:1.0 MACHINE_TYPE:Ender-3 V3 SE EXTRUDER_COUNT:1 UUID:cede2a2f-41a2-4748-9b12-c55c62f367ff Cap:SERIAL_XON_XOFF:0 Cap:BINARY_FILE_TRANSFER:0 Cap:EEPROM:1 Cap:VOLUMETRIC:1 Cap:AUTOREPORT_POS:0 Cap:AUTOREPORT_TEMP:1 Cap:PROGRESS:0 Cap:PRINT_JOB:1 Cap:AUTOLEVEL:1 Cap:RUNOUT:0 Cap:Z_PROBE:1 Cap:LEVELING_DATA:1 Cap:BUILD_PERCENT:0 Cap:SOFTWARE_POWER:0 Cap:TOGGLE_LIGHTS:0 Cap:CASE_LIGHT_BRIGHTNESS:0 Cap:EMERGENCY_PARSER:1 Cap:HOST_ACTION_COMMANDS:0 Cap:PROMPT_SUPPORT:0 Cap:SDCARD:1 Cap:REPEAT:0 Cap:SD_WRITE:1 Cap:AUTOREPORT_SD_STATUS:0 Cap:LONG_FILENAME:1 Cap:THERMAL_PROTECTION:1 Cap:MOTION_MODES:0 Cap:ARCS:1 Cap:BABYSTEPPING:1 Cap:CHAMBER_TEMPERATURE:0 Cap:COOLER_TEMPERATURE:0 Cap:MEATPACK:0";
+    }
+
+    bool files(){   //M20
+        //return "Begin file list\nLOL~1.GCO 0\nEnd file list";
+    }
+
+    bool longName(String path){ // M33 LOL~1.GCO
+        //return "/lol.gcode";
+    }
+
+    bool temps(){ //M105
+        //return "ok T:31.67 /0.00 B:31.79 /0.00 @:0 B@:0 FAN0@:0";
+    }
+
+    bool position(){
+        //return "X:-13.00 Y:-15.00 Z:0.00 E:0.00 Count X:-1040 Y:-1200 Z:0\nok";
+    }
+>>>>>>> b8607bc (quick commit -_-)
 
 }
 
