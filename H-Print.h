edc7a7326c4e0934d0936c9d0a8ef9f754742c7e
Vili <vili.kervinen.06@gmail.com>
2025-01-11 20:24:38 +0200

    quick commit -_-

--- H-Print.h
+++ H-Print.h
@@ -4,10 +4,9 @@
 #include "webServerClass.h"
 
 using namespace WebServerW;
-using namespace TD;
 namespace Handlers {
     void Print(){
-        printManager.start(server.arg("path"), false, 0);
+        dTD::printManager.start(server.arg("path"), false, 0);
         WebServerW::server.send(200, "text/html", "Printing...");
     }
 }
