2088bb59f5141f5d47dfc4aed29e1aa814203868
vili <vili.kervinen.06@gmail.com>
2025-04-23 23:14:36 +0300

    quick commit -_-

--- trioprintV2/trioprintV2/webServer.h
+++ trioprintV2/trioprintV2/webServer.h
@@ -77,10 +77,9 @@ void sendCommand(AsyncWebServerRequest* request) {
   request->send(200, "text/plain", "command sent");  // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
 }
 
-void ListFolder() {
-  auto e = SDW::listDir(server.arg("path"));
-  Debugger::print("listing files");
-  server.send(200, "application/json", e.c_str());
+void ListFolder(AsyncWebServerRequest* request) {
+  auto e = SDM::listDir(request->arg("path"));
+  request->send(200, "application/json", e.c_str());
 }
 
 }
