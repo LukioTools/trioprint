61b6f917870ce4905960601875e556ce04de9442
LukioTools <114951381+LukioTools@users.noreply.github.com>
2025-05-02 10:43:06 +0300

    .

--- trioprintV2/trioprintV2/stdExtension.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace std {
-  template<typename T, typename... Args>
-  std::unique_ptr<T> make_unique(Args&&... args) {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-  }
-}
